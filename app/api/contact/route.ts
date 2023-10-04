a0d72f580c6e24b4e57858ab0f2cb68a2e238fbc
Hugo Bessa <hugo@plusplus.co>
2023-10-03 23:14:46 -0300

    fixes contact route

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,39 +2,52 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { RecaptchaV3 } from 'express-recaptcha';
 import sgMail from '@sendgrid/mail';
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  const { name, email, subject, body, captchaResponse } = req.body;
+type ContactFormData = {
+  name: string;
+  email: string;
+  subject: string;
+  body: string;
+  captchaResponse: string;
+}
+
+async function sendEmail({ name, email, subject, body }: ContactFormData) {
+  try {
+    const sendGridAPIKey = process.env.SENDGRID_API_KEY as string;
+    sgMail.setApiKey(sendGridAPIKey);
+  
+    // Construct the email data
+    const emailData = {
+      to: 'hugo@bessa.me', // Replace with the recipient's email address
+      from: 'contact@bessa.me', // Replace with your sending email address
+      subject: subject,
+      text: `Name: ${name}\nEmail: ${email}\n\n${body}`,
+    };
+  
+    // Send the email using SendGrid
+    await sgMail.send(emailData);
+    return Response.json({ message: 'Message sent successfully.' });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ message: 'Internal server error.' }, {status: 500});
+  }
+}
+
+export async function POST(req: Request) {
+  const contactFormData: ContactFormData = await req.json();
 
   const recaptchaSiteKey = process.env.RECAPTCHA_SITE_KEY as string;
   const recaptchaSecretKey = process.env.RECAPTCHA_SECRET_KEY as string;
-  const sendGridAPIKey = process.env.SENDGRID_API_KEY as string;
 
   // Verify the captcha
-  const recaptchaClient = new RecaptchaV3(recaptchaSiteKey, recaptchaSecretKey);
-  recaptchaClient.verify(captchaResponse, async (error) => {
-    if (error) {
-      return Response.json({ message: 'Captcha verification failed.' }, {status: 400});
-    }
-
-    try {
-      // Replace 'your_sendgrid_api_key' with your actual SendGrid API key
-      sgMail.setApiKey(sendGridAPIKey);
-
-      // Construct the email data
-      const emailData = {
-        to: 'hugo@bessa.me', // Replace with the recipient's email address
-        from: 'contact@bessa.me', // Replace with your sending email address
-        subject: subject,
-        text: `Name: ${name}\nEmail: ${email}\n\n${body}`,
-      };
-
-      // Send the email using SendGrid
-      await sgMail.send(emailData);
-
-      return Response.json({ message: 'Message sent successfully.' });
-    } catch (error) {
-      console.error(error);
-      return Response.json({ message: 'Internal server error.' }, {status: 500});
-    }
-  });
+  if (process.env.NODE_ENV === "production") {
+    const recaptchaClient = new RecaptchaV3(recaptchaSiteKey, recaptchaSecretKey);
+    recaptchaClient.verify(contactFormData.captchaResponse, async (error) => {
+      if (error) {
+        return Response.json({ message: 'Captcha verification failed.' }, {status: 400});
+      }
+      return await sendEmail(contactFormData);
+    });
+  } else {
+    return await sendEmail(contactFormData);
+  }
 };
